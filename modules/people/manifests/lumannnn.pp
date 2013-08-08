13ea32bdca02df17654676e9aac1a4409bb9c50d
Lukas Ender <developer@lukasender.at>
2013-08-09 00:46:44 +0200

    removed the troublemakers...

--- modules/people/manifests/lumannnn.pp
+++ modules/people/manifests/lumannnn.pp
@@ -1,43 +1,34 @@
 class people::lumannnn {
-  include elasticsearch
-  include emacs
-  include java
-  include keepassx
-  include wget
+  # include elasticsearch
+  # include emacs
+  # include java
+  # include keepassx
+  # include wget
 
-  $home     = "/Users/${::boxen_user}"
-  $my       = "${home}/my"
-  #$dotfiles = "${my}/dotfiles"
+  # include osx::recovery_message { 'If this Mac is found, please send an email to hello@lukasender.at': }
+  # include osx::global::enable_keyboard_control_access	# enables the keyboard for navigating controls in dialogs
+  # include osx::global::expand_print_dialog				# expand the print dialog by default
+  # include osx::global::expand_save_dialog				# expand the save dialog by default
 
-  #repository { $dotfiles:
-  #  source  => 'lumannnn/dotfiles',
-  #  require => File[$my]
-  #}
+  # include osx::dock::dim_hidden_apps 					# dims icons of hidden apps
 
-  include osx::recovery_message { 'If this Mac is found, please send an email to hello@lukasender.at': }
-  include osx::global::enable_keyboard_control_access	# enables the keyboard for navigating controls in dialogs
-  include osx::global::expand_print_dialog				# expand the print dialog by default
-  include osx::global::expand_save_dialog				# expand the save dialog by default
+  # # osx::finder::show_external_hard_drives_on_desktop
+  # # osx::finder::show_hard_drives_on_desktop
+  # # osx::finder::show_mounted_servers_on_desktop
+  # # osx::finder::show_removable_media_on_desktop
+  # include osx::finder::show_all_on_desktop 				# does all of the above
 
-  include osx::dock::dim_hidden_apps 					# dims icons of hidden apps
+  # include osx::universal_access::ctrl_mod_zoom			# enables zoom by scrolling while holding Control
 
-  # osx::finder::show_external_hard_drives_on_desktop
-  # osx::finder::show_hard_drives_on_desktop
-  # osx::finder::show_mounted_servers_on_desktop
-  # osx::finder::show_removable_media_on_desktop
-  include osx::finder::show_all_on_desktop 				# does all of the above
+  # # Set the default value (35)
+  # include osx::global::key_repeat_delay
+  # # ... or set your own
+  # class { 'osx::global::key_repeat_delay':
+  #   delay => 10
+  # }
 
-  include osx::universal_access::ctrl_mod_zoom			# enables zoom by scrolling while holding Control
-
-  # Set the default value (35)
-  include osx::global::key_repeat_delay
-  # ... or set your own
-  class { 'osx::global::key_repeat_delay':
-    delay => 10
-  }
-
-  include osx::dock::icon_size
-  class { 'osx::dock::icon_size': 
-    size => 36
-  }
+  # include osx::dock::icon_size
+  # class { 'osx::dock::icon_size': 
+  #   size => 36
+  # }
 }
